fac8b88d92175c6612fd19a31187b57ef44fea14
howard_mac <howardch1993@outlook.com>
2018-01-26 17:08:43 +0800

    moderate

--- Python/qik_bind_py.py
+++ Python/qik_bind_py.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3 
-def qik_bind_py(f1 = '', f2 = '', outfile = 'csv'):
+def qik_bind_py(f1 = '', f2 = '', source_in_raw = False, outfile = 'csv'):
 
     import pandas as pd
     import numpy as np
@@ -8,7 +8,18 @@ def qik_bind_py(f1 = '', f2 = '', outfile = 'csv'):
     if f1 == '':
         print('input file1: ')
         f1 = input()
-    if f2 == '':
+    if source_in_raw == True:
+        # ctc spec
+        file = open('.txt', 'r', encoding = 'UTF-8')
+        raw = [x.rstrip('\n') for x in file]
+        file.close()
+        # 取第4行开始的第1列所有数据
+        data = [x.split('|')[0:] for x in raw[3:]]
+        # 在提取第一列数据
+        data = [x[0:1] for x in data[0:]]
+        f2 = pd.DataFrame(data)
+        del(data)
+    else :
         print('input file2: ')
         f2 = input()
     if outfile == '':
@@ -17,7 +28,12 @@ def qik_bind_py(f1 = '', f2 = '', outfile = 'csv'):
 
     # load data
     file1 = pd.read_table(f1, header = None)
-    file2 = pd.read_table(f2, header = None)
+    if source_in_raw == True:
+        file2 = f2
+        del(f2)
+    else :
+        file2 = pd.read_table(f2, header = None)
+        del(f2)
 
     # give colnames
     file1.columns = ['mb']
@@ -26,12 +42,14 @@ def qik_bind_py(f1 = '', f2 = '', outfile = 'csv'):
     # merge
     file = pd.concat([file1, file1.mb.isin(file2.mb)], axis = 1)
     file.columns = ['mb', 'flag']
+    del(file1)
+    del(file2)
 
     if outfile == 'csv':
         # outfile *.csv
         file.to_csv(f1 + '_marked.csv', encoding='utf-8', index = False)
     elif outfile == 'xlsx':
-        # outfile *.xlsx
+        # outfile *.xlsx maybe not work on windows
         file.to_excel(f1 + '_marked.xlsx', sheet_name = 'merged', index = False)
     else :
         print('unsupported type of output file. run default setting...')
@@ -42,4 +60,4 @@ import time
 start = time.clock()
 qik_bind_py(outfile = 'csv')
 end = time.clock()
-print('time_use: {} secs'.format('%.2f' % (s2 - s1)))
+print('time_use: {} secs'.format('%.2f' % (end - start)))
